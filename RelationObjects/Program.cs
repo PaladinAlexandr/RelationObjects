b99a21109cbe3d6d314aa3d0fbca6b6d76f6e55a
PaladinAlexandr <paladinalexandrwork@gmail.com>
2025-01-15 10:59:21 +0500

    Load

---
+++ RelationObjects/Program.cs
@@ -0,0 +1,66 @@
+﻿JetEngine jetEngine = new JetEngine();
+Rocket rocket = new Rocket(jetEngine);
+
+Trolleybus trolleybus = new Trolleybus();
+
+class User
+{
+    public int Id;
+    public string Name;
+
+    public User()
+    {
+
+    }
+}
+class Manager : User
+{
+    public string Company;
+    public Manager()
+    {
+
+    }
+}
+
+class Car : IMovable
+{
+    public float Speed { get; set; }
+
+    public void Move()
+    {
+
+    }
+}
+
+interface IMovable
+{
+    public float Speed { get; set; }
+
+    public void Move();
+}
+
+public class Trolleybus
+{
+    private ElectricEngine Engine;
+    public Trolleybus()
+    {
+        Engine = new ElectricEngine();
+    }
+}
+public class ElectricEngine
+{
+
+}
+
+public class Rocket
+{
+    private JetEngine Engine;
+    public Rocket(JetEngine engine)
+    {
+        Engine = engine;
+    }
+}
+public class JetEngine
+{
+
+}
